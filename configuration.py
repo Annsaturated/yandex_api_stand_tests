2a454a1dbedd241562090d23eb60cfe1b1721817
Anna <1113283013ann@gmail.com>
2024-06-14 13:25:27 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,9 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+# Значение должно быть скопировано из настроек или документации сервиса, к которому предоставляется доступ.
+# Пример значения: "https://api.example.com"
+URL_SERVICE = 'https://aa619d8a-335d-4ce9-a96b-d983bdf00092.serverhub.praktikum-services.ru'
+DOC_PATH = '/docs/'
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = '/api/db/resources/user_model.csv'
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = 'api/v1/products/kits/'
